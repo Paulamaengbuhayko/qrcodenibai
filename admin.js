88764e31048be023ce269a6ea5d7c525a2578df7
Paulamaengbuhayko <daleputoltae@gmail.com>
2025-05-30 22:03:42 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("loginForm").addEventListener("submit", function (event) {
-        event.preventDefault();
-        
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
 
-        console.log("Email entered:", email);
-        console.log("Password entered:", password);
-
-        if (email === "email" && password === "password") {
-            alert("Login Successful!");
-            window.location.href = "admin-dashboard.html"; 
-        } else {
-            alert("Invalid email or password!");
-        }
-    });
-});
